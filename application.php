e317c5838956aa8157b2a07ac6a950a504687f6a
Christian Mayer <christian@fox21.at>
2014-10-14 10:33:27 +0200

    Info added.

--- application.php
+++ application.php
@@ -5,8 +5,11 @@ require_once __DIR__.'/bootstrap.php';
 
 use Symfony\Component\Console\Application;
 
+use TheFox\Console\Command\InfoCommand;
 use TheFox\Console\Command\ServerCommand;
+use TheFox\Smtp\Smtpd;
 
-$application = new Application('SMTPd', '0.2.0-dev');
+$application = new Application(Smtpd::NAME, Smtpd::VERSION);
+$application->add(new InfoCommand());
 $application->add(new ServerCommand());
 $application->run();
---
+++ src/TheFox/Smtp/Smtpd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheFox\Smtp;
+
+class Smtpd{
+	
+	const NAME = 'SMTPd';
+	const VERSION = '0.2.x-dev';
+	const RELEASE = 3;
+	
+}
