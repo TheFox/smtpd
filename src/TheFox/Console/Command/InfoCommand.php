8845c0a7798daeb5055680dedbcb416c7ba5d40b
Christian Mayer <christian@fox21.at>
2014-10-15 22:07:13 +0200

    Text fix.

--- src/TheFox/Console/Command/InfoCommand.php
+++ src/TheFox/Console/Command/InfoCommand.php
@@ -21,9 +21,9 @@ class InfoCommand extends BasicCommand{
 	protected function configure(){
 		$this->setName('info');
 		$this->setDescription('Show infos.');
-		$this->addOption('name', null, InputOption::VALUE_NONE, 'Print the name this application.');
-		$this->addOption('name_lc', null, InputOption::VALUE_NONE, 'Print the lower-case name this application.');
-		$this->addOption('version_number', null, InputOption::VALUE_NONE, 'Print the version this application.');
+		$this->addOption('name', null, InputOption::VALUE_NONE, 'Prints the name of this application.');
+		$this->addOption('name_lc', null, InputOption::VALUE_NONE, 'Prints the lower-case name of this application.');
+		$this->addOption('version_number', null, InputOption::VALUE_NONE, 'Prints the version of this application.');
 	}
 	
 	protected function execute(InputInterface $input, OutputInterface $output){
