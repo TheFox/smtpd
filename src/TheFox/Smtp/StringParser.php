4e28b051070200933d1a0f0b8b26fdbeecc4902b
Christian Mayer <christian@fox21.at>
2014-11-19 02:00:36 +0100

    Clean up.

--- src/TheFox/Smtp/StringParser.php
+++ src/TheFox/Smtp/StringParser.php
@@ -4,8 +4,6 @@ namespace TheFox\Smtp;
 
 class StringParser{
 	
-	const DEBUG = 0;
-	
 	private $str = '';
 	private $len = 0;
 	private $argsMax;
@@ -20,11 +18,6 @@ class StringParser{
 		$this->argsMax = $argsMax;
 	}
 	
-	private function debug($text){
-		if(static::DEBUG)
-			fwrite(STDOUT, $text."\n");
-	}
-	
 	private function reset(){
 		$this->argsId = -1;
 		$this->args = array();
@@ -33,7 +26,6 @@ class StringParser{
 	
 	private function fixPrev(){
 		if($this->argsId >= 0){
-			#$this->debug('    fix old A /'.$this->args[$this->argsId].'/');
 			if($this->args[$this->argsId]
 				&& $this->args[$this->argsId][0] == '"'
 				&& substr($this->args[$this->argsId], -1) == '"'
@@ -44,31 +36,28 @@ class StringParser{
 					$this->argsLen = count($this->args);
 				}
 			}
-			#$this->debug('    fix old B /'.$this->args[$this->argsId].'/');
 		}
 	}
 	
 	private function charNew($char = ''){
 		if($this->argsMax === null || $this->argsLen < $this->argsMax){
 			$this->fixPrev();
-			#$this->debug('    new /'.$char.'/');
 			$this->argsId++;
 			$this->args[$this->argsId] = $char;
 			$this->argsLen = count($this->args);
 		}
-		else{
+		/*else{
 			$this->charAppend($char);
-		}
+		}*/
 	}
 	
 	private function charAppend($char){
-		if($this->argsId == -1){
-			$this->charNew($char);
-		}
-		else{
-			#$this->debug('    append /'.$char.'/');
+		if($this->argsId != -1){
 			$this->args[$this->argsId] .= $char;
 		}
+		/*else{
+			$this->charNew($char);
+		}*/
 	}
 	
 	public function parse(){
@@ -79,27 +68,21 @@ class StringParser{
 		$prevChar = ' ';
 		$endChar = '';
 		
-		#$this->debug('len: '.$this->len);
-		
 		for($pos = 0; $pos < $this->len; $pos++){
+			
 			$char = $str[$pos];
 			$nextChar = ($pos < $this->len - 1) ? $str[$pos + 1] : '';
 			
-			#$this->debug('raw '.$pos.'/'.$this->len.'['.$this->argsId.']: /'.$char.'/');
+			#fwrite(STDOUT, 'pos: '.$pos.' /'.$char.'/'."\n");
 			
 			if($in){
-				#$this->debug('    in ');
+				#fwrite(STDOUT, ' -> in'."\n");
 				if($char == $endChar){
-					#$this->debug('    is null: '.(int)($this->argsMax === null));
-					#$this->debug('    is end char: '.$this->argsLen.', '.(int)$this->argsMax);
-					
 					if($pos == $this->len - 1 || $this->argsMax === null || $this->argsLen < $this->argsMax){
 						if($char == '"'){
 							$this->charAppend($char);
 						}
-						
 						$in = false;
-						#$this->debug('    close ');
 					}
 					else{
 						$this->charAppend($char);
@@ -110,56 +93,36 @@ class StringParser{
 				}
 			}
 			else{
+				#fwrite(STDOUT, ' -> not in: '.(int)($this->argsMax === null).' '.$this->argsLen.' '.$this->argsMax."\n");
 				if($this->argsMax === null || $this->argsLen < $this->argsMax){
 					if($char == '"'){
-						#$this->debug('    new Ab (next /'.$nextChar.'/)');
 						$this->charNew($char);
 						$endChar = '"';
 						$in = true;
 					}
 					elseif($char == ' '){
-						/*if($nextChar == ' '){
-							#$this->debug('    new Ba (next / /)');
-						}
-						elseif($nextChar == '"'){
-							#$this->debug('    new Bb (next /"/)');
-						}
-						else{
-							#$this->debug('    new Bc (next /'.$nextChar.'/)');
-							$this->charNew();
-							$endChar = ' ';
-							$in = true;
-						}*/
 						if($nextChar != ' ' && $nextChar != '"'){
-							#$this->debug('    new Bc (next /'.$nextChar.'/)');
 							$this->charNew();
 							$endChar = ' ';
 							$in = true;
 						}
 					}
 					else{
-						#$this->debug('    new C');
 						$this->charNew($char);
 						$endChar = ' ';
 						$in = true;
 					}
 				}
-				else{
-					#$this->debug('    limit');
+				/*else{
+					fwrite(STDOUT, ' -> char append'."\n");
 					$this->charAppend($char);
-				}
+				}*/
 			}
 			
-			#$this->debug('    text /'.$this->args[$this->argsId].'/');
-			
 			$prevChar = $char;
-			
-			#sleep(1);
 		}
 		
 		$this->fixPrev();
-		#ve($this->args);
-		#exit();
 		
 		return $this->args;
 	}
