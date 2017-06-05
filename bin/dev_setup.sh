14d58c9387c0a11132d80c88232c609b99ee41fd
Christian Mayer <christian@fox21.at>
2017-06-05 15:11:42 +0200

    Moved Make targets to Bash scripts. #RN

---
+++ bin/dev_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_BASEDIR=$(dirname "$0")
+
+
+set -e
+cd "${SCRIPT_BASEDIR}/.."
+
+which php &> /dev/null || { echo 'ERROR: php not found in PATH'; exit 1; }
+which curl &> /dev/null || { echo 'ERROR: curl not found in PATH'; exit 1; }
+
+if [[ ! -f composer.phar ]] ; then
+    curl -sS https://getcomposer.org/installer | php
+    chmod u=rwx,go=rx composer.phar
+fi
+
+php composer.phar install --no-interaction
---
+++ bin/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_BASEDIR=$(dirname "$0")
+
+
+set -e
+cd "${SCRIPT_BASEDIR}/.."
+
+mkdir -p tmp
+vendor/bin/phpunit
