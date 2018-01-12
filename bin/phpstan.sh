c89ff952f5dfd946eb8d44858251b5b65067b2a7
Christian Mayer <christian@fox21.at>
2018-01-13 00:29:12 +0100

    Clean up.

--- bin/phpstan.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_BASEDIR=$(dirname "$0")
-
-
-set -e
-cd "${SCRIPT_BASEDIR}/.."
-
-vendor/bin/phpstan analyse --no-progress --level 5 src tests
--- bin/test.sh
+++ bin/test.sh
@@ -7,4 +7,14 @@ set -e
 cd "${SCRIPT_BASEDIR}/.."
 
 mkdir -p tmp
+
+# PHP Code Sniffer
+./vendor/bin/phpcs --config-set ignore_warnings_on_exit 1
+./vendor/bin/phpcs --config-show
+./vendor/bin/phpcs
+
+# PHPUnit
 vendor/bin/phpunit
+
+# PHPStan
+vendor/bin/phpstan analyse --no-progress --level 5 src tests
